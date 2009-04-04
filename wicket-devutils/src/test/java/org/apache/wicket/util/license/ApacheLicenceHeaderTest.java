eb38bd6369e8bf816dc56969df48809a51b5c7ca
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2009-04-04 16:35:35 +0000

    WICKET-2084 adding a version contributor to the debug bar adding the ApacheLicenseHeaderTest to devutils
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@761965 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-devutils/src/test/java/org/apache/wicket/util/license/ApacheLicenceHeaderTest.java
+++ wicket-devutils/src/test/java/org/apache/wicket/util/license/ApacheLicenceHeaderTest.java
@@ -14,30 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.wicket.devutils.inspector;
+package org.apache.wicket.util.license;
 
-import org.apache.wicket.Session;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.util.lang.Bytes;
 
-public class SessionTotalSizeModel extends LoadableDetachableModel<Bytes> {
+/**
+ * Test that the license headers are in place in this project. The tests are run from
+ * {@link ApacheLicenseHeaderTestCase}, but you can add project specific tests here if needed.
+ * 
+ * @author Frank Bille Jensen (frankbille)
+ */
+public class ApacheLicenceHeaderTest extends ApacheLicenseHeaderTestCase
+{
 
-	private static final long serialVersionUID = 1L;
-
-	private Session session;
-
-	public SessionTotalSizeModel(Session session) {
-		this.session = session;
-	}
-
-	@Override
-	protected Bytes load() {
-		return Bytes.bytes(session.getSizeInBytes());
-	}
-
-	@Override
-	protected void onDetach() {
-		super.onDetach();
-		this.session = null;
+	/**
+	 * Construct.
+	 */
+	public ApacheLicenceHeaderTest()
+	{
+		// addHeaders = true;
 	}
 }
