sandreWhite/wicket

WICKET-5735 Propagate fileSizeMax attribute to org.apache.commons.fileupload.FileUploadBase
Rework the Pull Request to be backward compatible so it can be applied to 6.x branch too.
Intorduce a special interpolator variable ${fileMaxSize} for its i18n message.
Fix whitespaces and imports order

wicket-core/src/main/java/org/apache/wicket/markup/html/form/Form.java
@@ -44,6 +44,7 @@ import org.apache.wicket.markup.html.form.validation.FormValidatorAdapter;
 import org.apache.wicket.markup.html.form.validation.IFormValidator;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.servlet.MultipartServletWebRequest;
 import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
 import org.apache.wicket.request.IRequestHandler;
 import org.apache.wicket.request.IRequestMapper;
@@ -52,7 +53,6 @@ import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.UrlRenderer;
-import org.apache.wicket.request.http.WebRequest;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.encoding.UrlDecoder;
 import org.apache.wicket.util.lang.Args;
@@ -254,6 +254,7 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	private static final String UPLOAD_FAILED_RESOURCE_KEY = "uploadFailed";
 
 	private static final String UPLOAD_TOO_LARGE_RESOURCE_KEY = "uploadTooLarge";
+	private static final String UPLOAD_SINGLE_FILE_TOO_LARGE_RESOURCE_KEY = "uploadSingleFileTooLarge";
 
 	/**
 	 * Any default IFormSubmittingComponent. If set, a hidden submit component will be rendered
@@ -273,6 +274,11 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	 * {@link org.apache.wicket.settings.ApplicationSettings#getDefaultMaximumUploadSize()} is used.
 	 */
 	private Bytes maxSize = null;
+
+	/**
+	 * Maximum size of file of upload in bytes (if there are more than one) in request.
+	 */
+	private Bytes fileMaxSize;
 
 	/** True if the form has enctype of multipart/form-data */
 	private short multiPart = 0;
@@ -572,6 +578,14 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 			return getApplication().getApplicationSettings().getDefaultMaximumUploadSize();
 		}
 		return maxSize[0];
+	}
+
+	/**
+	 * Gets maximum size for each file of an upload.
+	 * @return
+	 */
+	public Bytes getFileMaxSize() {
+		return fileMaxSize;
 	}
 
 	/**
@@ -1036,9 +1050,17 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	 * @param maxSize
 	 *            The maximum size
 	 */
-	public final void setMaxSize(final Bytes maxSize)
+	public void setMaxSize(final Bytes maxSize)
 	{
 		this.maxSize = maxSize;
+	}
+
+	/**
+	 * Sets maximum size of each file in upload request.
+	 * @param fileMaxSize
+	 */
+	public void setFileMaxSize(Bytes fileMaxSize) {
+		this.fileMaxSize = fileMaxSize;
 	}
 
 	/**
@@ -1379,8 +1401,9 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 			try
 			{
 				ServletWebRequest request = (ServletWebRequest)getRequest();
-				final WebRequest multipartWebRequest = request.newMultipartWebRequest(getMaxSize(),
-					getPage().getId());
+				final MultipartServletWebRequest multipartWebRequest = request.newMultipartWebRequest(getMaxSize(), getPage().getId());
+				multipartWebRequest.setFileMaxSize(getFileMaxSize());
+				multipartWebRequest.parse();
 				// TODO: Can't this be detected from header?
 				getRequestCycle().setRequest(multipartWebRequest);
 			}
@@ -1390,6 +1413,7 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 				final Map<String, Object> model = new HashMap<>();
 				model.put("exception", fux);
 				model.put("maxSize", getMaxSize());
+				model.put("fileMaxSize", getFileMaxSize());
 
 				onFileUploadException(fux, model);
 
@@ -1416,6 +1440,11 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 		if (e instanceof FileUploadBase.SizeLimitExceededException)
 		{
 			String msg = getString(UPLOAD_TOO_LARGE_RESOURCE_KEY, Model.ofMap(model));
+			error(msg);
+		}
+		else if (e instanceof FileUploadBase.FileSizeLimitExceededException)
+		{
+			String msg = getString(UPLOAD_SINGLE_FILE_TOO_LARGE_RESOURCE_KEY, Model.ofMap(model));
 			error(msg);
 		}
 		else

wicket-core/src/main/java/org/apache/wicket/protocol/http/servlet/MultipartServletWebRequest.java
@@ -22,9 +22,12 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.wicket.protocol.http.IMultipartWebRequest;
 import org.apache.wicket.request.IRequestParameters;
 import org.apache.wicket.request.Url;
+import org.apache.wicket.util.lang.Args;
+import org.apache.wicket.util.lang.Bytes;
 
 /**
  * Servlet specific WebRequest subclass for multipart content uploads.
@@ -35,6 +38,15 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 	implements
 		IMultipartWebRequest
 {
+	/**
+	 *  Maximum size of all uploaded files in bytes in a request.
+	 */
+	private Bytes maxSize;
+
+	/**
+	 *  Maximum size of file of upload in bytes (if there are more than one) in a request.
+	 */
+	private Bytes fileMaxSize;
 
 	/**
 	 * Construct.
@@ -60,11 +72,19 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 		super(httpServletRequest, filterPrefix, url);
 	}
 
+	public abstract void parse() throws FileUploadException;
+
 	@Override
 	public ServletWebRequest cloneWithUrl(Url url)
 	{
 		return new MultipartServletWebRequest(getContainerRequest(), getFilterPrefix(), url)
 		{
+			@Override
+			public void parse() throws FileUploadException
+			{
+				MultipartServletWebRequest.this.parse();
+			}
+
 			@Override
 			public List<FileItem> getFile(String fieldName)
 			{
@@ -83,5 +103,26 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 				return MultipartServletWebRequest.this.getPostParameters();
 			}
 		};
+	}
+
+	public Bytes getMaxSize()
+	{
+		return maxSize;
+	}
+
+	public void setMaxSize(Bytes maxSize)
+	{
+		Args.notNull(maxSize, "maxSize");
+		this.maxSize = maxSize;
+	}
+
+	public Bytes getFileMaxSize()
+	{
+		return fileMaxSize;
+	}
+
+	public void setFileMaxSize(Bytes fileMaxSize)
+	{
+		this.fileMaxSize = fileMaxSize;
 	}
 }

ShoOgino/wicketFile

wicket-core/src/main/java/org/apache/wicket/markup/html/form/Form.java
@@ -44,6 +44,7 @@ import org.apache.wicket.markup.html.form.validation.FormValidatorAdapter;
 import org.apache.wicket.markup.html.form.validation.IFormValidator;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.servlet.MultipartServletWebRequest;
 import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
 import org.apache.wicket.request.IRequestHandler;
 import org.apache.wicket.request.IRequestMapper;
@@ -52,7 +53,6 @@ import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.UrlRenderer;
-import org.apache.wicket.request.http.WebRequest;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.encoding.UrlDecoder;
 import org.apache.wicket.util.lang.Args;
@@ -254,6 +254,7 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	private static final String UPLOAD_FAILED_RESOURCE_KEY = "uploadFailed";
 
 	private static final String UPLOAD_TOO_LARGE_RESOURCE_KEY = "uploadTooLarge";
+	private static final String UPLOAD_SINGLE_FILE_TOO_LARGE_RESOURCE_KEY = "uploadSingleFileTooLarge";
 
 	/**
 	 * Any default IFormSubmittingComponent. If set, a hidden submit component will be rendered
@@ -273,6 +274,11 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	 * {@link org.apache.wicket.settings.ApplicationSettings#getDefaultMaximumUploadSize()} is used.
 	 */
 	private Bytes maxSize = null;
+
+	/**
+	 * Maximum size of file of upload in bytes (if there are more than one) in request.
+	 */
+	private Bytes fileMaxSize;
 
 	/** True if the form has enctype of multipart/form-data */
 	private short multiPart = 0;
@@ -572,6 +578,14 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 			return getApplication().getApplicationSettings().getDefaultMaximumUploadSize();
 		}
 		return maxSize[0];
+	}
+
+	/**
+	 * Gets maximum size for each file of an upload.
+	 * @return
+	 */
+	public Bytes getFileMaxSize() {
+		return fileMaxSize;
 	}
 
 	/**
@@ -1036,9 +1050,17 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 	 * @param maxSize
 	 *            The maximum size
 	 */
-	public final void setMaxSize(final Bytes maxSize)
+	public void setMaxSize(final Bytes maxSize)
 	{
 		this.maxSize = maxSize;
+	}
+
+	/**
+	 * Sets maximum size of each file in upload request.
+	 * @param fileMaxSize
+	 */
+	public void setFileMaxSize(Bytes fileMaxSize) {
+		this.fileMaxSize = fileMaxSize;
 	}
 
 	/**
@@ -1379,8 +1401,10 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 			try
 			{
 				ServletWebRequest request = (ServletWebRequest)getRequest();
-				final WebRequest multipartWebRequest = request.newMultipartWebRequest(getMaxSize(),
-					getPage().getId());
+				final MultipartServletWebRequest multipartWebRequest = request.newMultipartWebRequest(getMaxSize(), getPage().getId());
+				multipartWebRequest.setFileMaxSize(getFileMaxSize());
+				multipartWebRequest.parseFileParts();
+
 				// TODO: Can't this be detected from header?
 				getRequestCycle().setRequest(multipartWebRequest);
 			}
@@ -1390,6 +1414,7 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 				final Map<String, Object> model = new HashMap<>();
 				model.put("exception", fux);
 				model.put("maxSize", getMaxSize());
+				model.put("fileMaxSize", getFileMaxSize());
 
 				onFileUploadException(fux, model);
 
@@ -1416,6 +1441,11 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener,
 		if (e instanceof FileUploadBase.SizeLimitExceededException)
 		{
 			String msg = getString(UPLOAD_TOO_LARGE_RESOURCE_KEY, Model.ofMap(model));
+			error(msg);
+		}
+		else if (e instanceof FileUploadBase.FileSizeLimitExceededException)
+		{
+			String msg = getString(UPLOAD_SINGLE_FILE_TOO_LARGE_RESOURCE_KEY, Model.ofMap(model));
 			error(msg);
 		}
 		else

wicket-core/src/main/java/org/apache/wicket/protocol/http/servlet/MultipartServletWebRequest.java
@@ -22,9 +22,12 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.wicket.protocol.http.IMultipartWebRequest;
 import org.apache.wicket.request.IRequestParameters;
 import org.apache.wicket.request.Url;
+import org.apache.wicket.util.lang.Args;
+import org.apache.wicket.util.lang.Bytes;
 
 /**
  * Servlet specific WebRequest subclass for multipart content uploads.
@@ -35,6 +38,15 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 	implements
 		IMultipartWebRequest
 {
+	/**
+	 *  Maximum size of all uploaded files in bytes in a request.
+	 */
+	private Bytes maxSize;
+
+	/**
+	 *  Maximum size of file of upload in bytes (if there are more than one) in a request.
+	 */
+	private Bytes fileMaxSize;
 
 	/**
 	 * Construct.
@@ -60,11 +72,25 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 		super(httpServletRequest, filterPrefix, url);
 	}
 
+	/**
+	 * Parses the multipart body of the request.
+	 *
+	 * @throws FileUploadException
+	 * @since 6.18.0
+	 */
+	public abstract void parseFileParts() throws FileUploadException;
+
 	@Override
 	public ServletWebRequest cloneWithUrl(Url url)
 	{
 		return new MultipartServletWebRequest(getContainerRequest(), getFilterPrefix(), url)
 		{
+			@Override
+			public void parseFileParts() throws FileUploadException
+			{
+				MultipartServletWebRequest.this.parseFileParts();
+			}
+
 			@Override
 			public List<FileItem> getFile(String fieldName)
 			{
@@ -83,5 +109,26 @@ public abstract class MultipartServletWebRequest extends ServletWebRequest
 				return MultipartServletWebRequest.this.getPostParameters();
 			}
 		};
+	}
+
+	public Bytes getMaxSize()
+	{
+		return maxSize;
+	}
+
+	public void setMaxSize(Bytes maxSize)
+	{
+		Args.notNull(maxSize, "maxSize");
+		this.maxSize = maxSize;
+	}
+
+	public Bytes getFileMaxSize()
+	{
+		return fileMaxSize;
+	}
+
+	public void setFileMaxSize(Bytes fileMaxSize)
+	{
+		this.fileMaxSize = fileMaxSize;
 	}
 }