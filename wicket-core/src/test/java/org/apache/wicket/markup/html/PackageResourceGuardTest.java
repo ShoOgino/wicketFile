apache/wicket

WICKET-5851 Fix PackageResourceTest and PackageResourceGuardTest under Windows

wicket-core/src/test/java/org/apache/wicket/markup/html/PackageResourceGuardTest.java
@@ -54,16 +54,21 @@ public class PackageResourceGuardTest extends WicketTestCase
 		guard.setAllowAccessToRootResources(false);
 
 		assertTrue(guard.acceptAbsolutePath("/test/test.js"));
+		assertTrue(guard.acceptAbsolutePath("/test/.java"));
 		assertFalse(guard.acceptAbsolutePath("/test.js"));
 
-		if ("\\".equals(File.pathSeparator))
+		if ("\\".equals(File.separator))
 		{
+			assertTrue(guard.acceptAbsolutePath("c:\\test\\org\\apache\\.java"));
+			assertTrue(guard.acceptAbsolutePath("\\test\\org\\apache\\.java"));
 			assertTrue(guard.acceptAbsolutePath("c:\\test\\org\\apache\\test.js"));
 			assertTrue(guard.acceptAbsolutePath("\\test\\org\\apache\\test.js"));
 			assertFalse(guard.acceptAbsolutePath("c:\\test.js"));
 			assertFalse(guard.acceptAbsolutePath("\\test.js"));
 
 			// java also generates file paths with '/' on windows
+			assertTrue(guard.acceptAbsolutePath("c:/test/org/apache/.java"));
+			assertTrue(guard.acceptAbsolutePath("/test/org/apache/.java"));
 			assertTrue(guard.acceptAbsolutePath("c:/test/org/apache/test.js"));
 			assertTrue(guard.acceptAbsolutePath("/test/org/apache/test.js"));
 			assertFalse(guard.acceptAbsolutePath("c:/test.js"));

ShoOgino/wicketFile

(cherry picked from commit 2ed52c012c5d84c9dfcb5ffb571b9541c0575073)

wicket-core/src/test/java/org/apache/wicket/markup/html/PackageResourceGuardTest.java
@@ -54,16 +54,21 @@ public class PackageResourceGuardTest extends WicketTestCase
 		guard.setAllowAccessToRootResources(false);
 
 		assertTrue(guard.accept("/test/test.js"));
+		assertTrue(guard.accept("/test/.java"));
 		assertFalse(guard.accept("/test.js"));
 
-		if ("\\".equals(File.pathSeparator))
+		if ("\\".equals(File.separator))
 		{
+			assertTrue(guard.accept("c:\\test\\org\\apache\\.java"));
+			assertTrue(guard.accept("\\test\\org\\apache\\.java"));
 			assertTrue(guard.accept("c:\\test\\org\\apache\\test.js"));
 			assertTrue(guard.accept("\\test\\org\\apache\\test.js"));
 			assertFalse(guard.accept("c:\\test.js"));
 			assertFalse(guard.accept("\\test.js"));
 
 			// java also generates file paths with '/' on windows
+			assertTrue(guard.accept("c:/test/org/apache/.java"));
+			assertTrue(guard.accept("/test/org/apache/.java"));
 			assertTrue(guard.accept("c:/test/org/apache/test.js"));
 			assertTrue(guard.accept("/test/org/apache/test.js"));
 			assertFalse(guard.accept("c:/test.js"));